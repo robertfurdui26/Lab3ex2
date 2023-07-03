ced4ea85734a5c5a1492c6be7533896ad35a689a
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-07-03 15:33:33 +0300

    Add project files.

---
+++ Lab3ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Ex 2
+//Scrieti o functie care va determina daca un numar este sau nu numar prim. Apelati-o si afisati-i rezultatul
+
+Console.WriteLine("introduce un numar: ");int numar = int.Parse(Console.ReadLine());bool rezultat= IsNumarPrim(numar);if (rezultat)
+{
+    Console.WriteLine("Numarul este prim");
+}
+else
+{
+    Console.WriteLine("Nu este prim");
+}static bool IsNumarPrim(int numar)
+{
+    if(numar<2) return false;
+
+    for(int i = 2;i < Math.Sqrt(numar); i++)
+    {
+        if(numar % i == 0)
+            return true;
+    }
+    return false;
+}
